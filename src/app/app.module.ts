cbb6189063e80c9c035287eed05ebc5923842d08
Germán <gapag.44@hotmail.com>
2020-11-18 17:01:58 -0500

    Enrutamiento de paginas tipo SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
+
 // For MDB Angular Free
 import { CarouselModule, WavesModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule,Routes } from '@angular/router';
+import { LoginModule } from './login/login.module';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+  {
+    path : 'login',
+    component : LoginComponent
+  }
+
+];
+
 
 @NgModule({
   declarations: [
@@ -16,7 +30,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    LoginModule,
+    RouterModule.forRoot(routes)
+
+
+
     
     
 
